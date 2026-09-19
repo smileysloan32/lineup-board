af6ae23c8a1e779eba018a454319eebb63521765
smileysloan32 <smileysloan32@users.noreply.github.com>
2026-09-19 03:31:03 +0000

    Rotation and Stats tabs

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='lineup-v9';const FILES=['./','index.html','manifest.webmanifest?v=3','icon-192.png','icon-512.png'];
+const C='lineup-v10';const FILES=['./','index.html','manifest.webmanifest?v=3','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
